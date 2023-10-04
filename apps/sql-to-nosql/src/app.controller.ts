8bd8046db8b27fb0187777fc438827e2b4fb0c66
Vahid V. <vhid.vz@gmail.com>
2023-10-04 09:55:00 +0330

    update

--- apps/sql-to-nosql/src/app.controller.ts
+++ apps/sql-to-nosql/src/app.controller.ts
@@ -1,12 +1,14 @@
+import { Controller, UseInterceptors, UsePipes } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { Controller, UseInterceptors } from '@nestjs/common';
 import { LoggerInterceptor } from '@app/common/interceptors';
 import { SentryInterceptor } from '@ntegral/nestjs-sentry';
+import { ValidationPipe } from '@app/common/pipes';
 
 import { MysqlSourceDto } from './dto';
 import { AppService } from './app.service';
 
 @Controller()
+@UsePipes(ValidationPipe)
 @UseInterceptors(LoggerInterceptor, new SentryInterceptor())
 export class AppController {
   constructor(private readonly appService: AppService) {}
