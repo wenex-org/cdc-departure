25f588bd1c9d367179b01a261c56d39e948adae6
Vahid V. <vhid.vz@gmail.com>
2023-10-04 22:53:13 +0330

    update

--- apps/nosql-to-sql/src/modules/fortest/fortest.service.ts
+++ apps/nosql-to-sql/src/modules/fortest/fortest.service.ts
@@ -1,12 +1,15 @@
+import { date, logger } from '@app/common/utils';
 import { Injectable } from '@nestjs/common';
 
 import { FortestRepository } from './fortest.repository';
 
 @Injectable()
 export class FortestService {
+  private readonly log = logger(FortestService.name);
+
   constructor(readonly fortestRepository: FortestRepository) {}
 
   migrate(payload: any) {
-    console.log(payload);
+    this.log.get(this.migrate.name).debug(date(`payload received with data %j`), payload);
   }
 }
